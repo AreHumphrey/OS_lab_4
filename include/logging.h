c7368ab8db4bfd084d3f8a3708fc6909e9f215c0
Dina <ajjbdfnhfjkvn@gmail.com>
2026-01-31 20:12:17 +1000

    Update logging.h

--- include/logging.h
+++ include/logging.h
@@ -8,16 +8,15 @@
 
 namespace lab4 {
 
-// Форматирование измерения в строку для лога
+
 std::string format_line(const Sample& s);
 
-// Парсинг временной метки из строки лога
+
 std::optional<std::int64_t> parse_epoch_ms(const std::string& line);
 
-// Добавление строки в конец файла
+
 void append_line(const std::string& path, const std::string& line);
 
-// Очистка лога: удаление записей старше cutoff_ms
 void prune_log(const std::string& path, std::int64_t cutoff_ms);
 
 }  // namespace lab4
